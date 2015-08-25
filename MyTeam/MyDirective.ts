c69df4556655519fba37e5f950b50c53aa39553e
AlexZhidkov <azhidkov@gmail.com>
2015-08-25 11:52:31 +0800

    google.maps.places.Autocomplete

--- MyTeam/MyDirective.ts
+++ MyTeam/MyDirective.ts
@@ -2,10 +2,22 @@
     angular.module("teamBuilder");
 
     class MyDirective {
-//    public restrict: {"E"};
-        public link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => void;
-        public template = '<div>az test</div>';
-        public scope = {};
+        restrict = 'E';
+        replace = true;
+        //link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => void;
+        template = '<input id="google_places_ac" name="google_places_ac" type="text" class="input-block-level"/>';
+        scope = { location:'='};
+        link = (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+            var input = <HTMLInputElement>(document.getElementById('google_places_ac'));
+            //var autocomplete = new google.maps.places.Autocomplete($("#google_places_ac")[0], {});
+            var autocomplete = new google.maps.places.Autocomplete(input, {});
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                var place = autocomplete.getPlace();
+                //scope.location = place.geometry.location.lat() + ',' + place.geometry.location.lng();
+                scope["placeId"] = place.place_id;
+                scope.$apply();
+            });
+        }
 
         constructor( /*list of dependencies*/) {
             // It's important to add `link` to the prototype or you will end up with state issues.
