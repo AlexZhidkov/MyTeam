f38a0157aebd6a80c954386e3659da733dc4a96a
AlexZhidkov <azhidkov@gmail.com>
2015-08-26 16:37:35 +0800

    removed routing

--- MyTeam/app.ts
+++ MyTeam/app.ts
@@ -3,27 +3,7 @@
 /// <reference path="Scripts/typings/google.geolocation/google.geolocation.d.ts"/>
 /// <reference path="Scripts/typings/googlemaps/google.maps.d.ts"/>
 module App {
-    var main = angular.module("teamBuilder", ["ngRoute", "ngMaterial", "firebase", "ezfb", "teamBuilder.repositoryService", "teamBuilder.facebookService", "teamBuilder.game"]);
-
-    function routeConfig($routeProvider: ng.route.IRouteProvider): void {
-        $routeProvider
-            .when("/",
-                {
-                    templateUrl: "mainView.html",
-                    controller: "GameListCtrl as vm"
-                })
-            .when("/newGame",
-                {
-                    templateUrl: "addNewGameView.html",
-                    controller: "GameListCtrl as vm"
-                })
-            .when("/player",
-                {
-                    templateUrl: "playerProfileView.html",
-                    controller: "PlayerProfileCtrl as vm"
-                })
-            .otherwise("/");
-    }
+    var main = angular.module("teamBuilder", ["ngMaterial", "firebase", "ezfb", "teamBuilder.repositoryService", "teamBuilder.facebookService", "teamBuilder.game"]);
 
     function facebookConfig(ezfbProvider): void {
         ezfbProvider.setInitParams({
@@ -32,85 +12,7 @@ module App {
         });
     }
 
-/*
-    function facebookRun($rootScope, $window, sAuth): void {
-
-        $rootScope.user = {};
-
-        $window.fbAsyncInit = function () {
-            // Executed when the SDK is loaded
-
-            FB.init({ 
-
-                /* 
-                 The app id of the web app;
-                 To register a new app visit Facebook App Dashboard
-                 ( https://developers.facebook.com/apps/ ) 
-                #1#
-
-                appId: '***************', 
-
-                /* 
-                 Adding a Channel File improves the performance 
-                 of the javascript SDK, by addressing issues 
-                 with cross-domain communication in certain browsers. 
-                #1#
-
-                channelUrl: 'app/channel.html', 
-
-                /* 
-                 Set if you want to check the authentication status
-                 at the start up of the app 
-                #1#
-
-                status: true, 
-
-                /* 
-                 Enable cookies to allow the server to access 
-                 the session 
-                #1#
-
-                cookie: true, 
-
-                /* Parse XFBML #1#
-
-                xfbml: true
-            });
-
-            sAuth.watchAuthenticationStatusChange();
-
-        };
-
-        // Are you familiar to IIFE ( http://bit.ly/iifewdb ) ?
-
-        (function (d) {
-            // load the Facebook javascript SDK
-
-            var js,
-                id = 'facebook-jssdk',
-                ref = d.getElementsByTagName('script')[0];
-
-            if (d.getElementById(id)) {
-                return;
-            }
-
-            js = d.createElement('script');
-            js.id = id;
-            js.async = true;
-            js.src = "//connect.facebook.net/en_US/all.js";
-
-            ref.parentNode.insertBefore(js, ref);
-
-        } (document));
-
-    }
-*/
-
-    main.config(routeConfig);
     main.config(facebookConfig);
-    //main.run(facebookRun);
 
-    routeConfig.$inject = ["$routeProvider"];
     facebookConfig.$inject = ["ezfbProvider"];
-    //facebookRun.$inject = ['$rootScope', '$window', 'srvAuth'];
 }
